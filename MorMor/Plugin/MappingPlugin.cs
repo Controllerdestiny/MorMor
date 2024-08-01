0042c33109738d0836dda14703525a753b15f29b
Controllerdestiny <523321293@qq.co>
2024-08-02 01:27:32 +0800

    更新: 公开函数

--- MorMor/Plugin/MappingPlugin.cs
+++ MorMor/Plugin/MappingPlugin.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 
 namespace MorMor.Plugin;
 
-internal class MappingPlugin
+public class MappingPlugin
 {
     public readonly static List<MorMorPlugin> Instances = new();
 
